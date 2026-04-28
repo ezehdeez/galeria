d01d86bbbfc4fd06e97deccf225059d5c356dfc1
EzequielHdezPoleo <alu0101735399@ull.edu.es>
2026-04-28 22:33:17 +0100

    Add galleryData.js with initial photo entries

---
+++ src/data/galleryData.js
@@ -0,0 +1,18 @@
+export const galleryData = [
+  [
+    {
+      url: "/mi-galeria/fotos/sala1/foto1.jpg",
+      caption: "El primer día que nos perdimos juntos y encontramos algo mejor.",
+    },
+    {
+      url: "/mi-galeria/fotos/sala1/foto2.jpg",
+      caption: "Ese atardecer que prometimos repetir y nunca repetimos igual.",
+    },
+  ],
+  [
+    {
+      url: "/mi-galeria/fotos/sala2/foto1.jpg",
+      caption: "Año uno. Ya sabíamos que esto era para siempre.",
+    },
+  ],
+];
